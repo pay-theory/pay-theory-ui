dcc41b8b5d76aeb05344b02222f3557d30ed6d89
Austin Zani <austin@paytheory.com>
2020-08-31 02:41:55 +0000

    updated the table head function to select all rows

--- src/common/InnerTable/children/HeaderRow.jsx
+++ src/common/InnerTable/children/HeaderRow.jsx
@@ -44,7 +44,7 @@ const HeaderRow = (props) => {
               selected.length > 0 && selected.length === tableData.length,
             onChange: (e) => {
               if (e.target.checked) {
-                setSelected([...new Array(tableData.length).keys()]);
+                setSelected(Array.from(Array(tableData.length).keys()));
               } else {
                 setSelected([]);
               }
