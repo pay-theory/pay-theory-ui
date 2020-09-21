703d63ac92ee407c28f743b3d2940f7f0352c7a3
Austin Zani <austin@paytheory.com>
2020-09-21 17:41:25 +0000

    updated the export CSV to format amounts with decimal and $

--- src/common/generalUtils.js
+++ src/common/generalUtils.js
@@ -1,18 +1,28 @@
+ const formatFee = (fee) => {
+  return fee < 0 ? `-$${(Math.abs(fee) / 100).toFixed(2)}` : `$${(fee / 100).toFixed(2)}`;
+ };
+
+ const formatString = (string) => {
+  return string ? string[0] + string.substring(1).toLowerCase() : '';
+ }
+
+ export { formatFee, formatString }
+
  function arrayToCSV(objArray) {
-     const array =
-         typeof objArray !== "object" ? JSON.parse(objArray) : objArray;
-     let str =
-         `${Object.keys(array[0])
+  const array =
+   typeof objArray !== "object" ? JSON.parse(objArray) : objArray;
+  let str =
+   `${Object.keys(array[0])
         .map((value) => `"${value}"`)
         .join(",")}` + "\r\n";
 
-     return array.reduce((str, next) => {
-         str +=
-             `${Object.values(next)
-          .map((value) => `"${value}"`)
+  return array.reduce((str, next) => {
+   str +=
+    `${Object.keys(next)
+          .map((value) => `"${value.includes("amount") ? formatFee(next[value]) : next[value]}"`)
           .join(",")}` + "\r\n";
-         return str;
-     }, str);
+   return str;
+  }, str);
  }
 
  export { arrayToCSV }
