98d590da65ef6cef40daf14e2e6a165243ea9d0c
Austin Zani <austinzani@gmail.com>
2020-12-14 09:21:23 -0500

    added feeMode component to library

--- src/admin/FeeModeTab/index.test.jsx
+++ src/admin/FeeModeTab/index.test.jsx
@@ -4,30 +4,12 @@ import '@testing-library/jest-dom/extend-expect'
 
 import { render, fireEvent } from '@testing-library/react'
 
-import AccountsTab from '.'
+import FeeModeTab from '.'
 
-import * as BooksHooks from '../../hooks'
+import { feeModes } from '../../test-data'
 
-import { accounts } from '../../test-data'
+test('display feeModes tab', async () => {
+    const { queryByTestId } = render(<FeeModeTab feeModes={feeModes} />)
 
-const viewAccount = jest.fn()
-const deleteAccount = jest.fn()
-
-// test('display accounts tab', async () => {
-//     const { queryAllByTestId } = render(
-//         <BooksHooks.context.accounts.Provider value={accounts}>
-//             <AccountsTab
-//                 id='test-id'
-//                 visibility='visible'
-//                 viewAccount={viewAccount}
-//                 deleteAccount={deleteAccount}
-//             />
-//         </BooksHooks.context.accounts.Provider>
-//     )
-
-//     fireEvent.click(queryAllByTestId('view-action')[0])
-//     expect(viewAccount).toHaveBeenCalledTimes(1)
-
-//     fireEvent.click(queryAllByTestId('delete-action')[0])
-//     expect(deleteAccount).toHaveBeenCalledTimes(1)
-// })
+    expect(queryByTestId('service-fee')).toBeInTheDocument()
+})
