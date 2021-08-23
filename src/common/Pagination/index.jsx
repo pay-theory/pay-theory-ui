eacbef9d3b01bd756f54507684cf7a28fa7fc4e1
Austin Zani <austinzani@gmail.com>
2021-08-23 16:53:27 -0400

    updated the pagination tool

--- src/common/Pagination/index.jsx
+++ src/common/Pagination/index.jsx
@@ -1,123 +1,126 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 const Pagination = ({ page, setPage, total }) => {
-  const [pages, setPages] = useState([]);
+    const [pages, setPages] = useState([])
 
-  const updatePages = () => {
-    if (total < 6) {
-      const result = Array.from(Array(total).keys());
-      return result.map((i) => ++i);
-    }
-    else if (page < 4) {
-      return [1, 2, 3, 4, 5, "..."];
-    }
-    else if (page > total - 3) {
-      return ["...", total - 4, total - 3, total - 2, total - 1, total];
-    }
-    else {
-      return ["...", page - 2, page - 1, page, page + 1, page + 2, "..."];
-    }
-  };
-
-  useEffect(() => {
-    setPages(updatePages());
-  }, [page, total]);
-
-  return (
-    <div className="pagination">
-      {page === 1 ? (
-        <div className="spacer" />
-      ) : (
-        <span>
-          <i className="fas fa-chevron-double-left" onClick={() => setPage(1)} data-testid="jump-beginning" />
-          <i className="fas fa-chevron-left" onClick={() => setPage(--page)} data-testid="previous" />
-        </span>
-      )}
-      {pages.map((item, index) => {
-        if (typeof item === "number") {
-          return (
-            <p
-              className={`number ${page === item ? "active" : ""}`}
-              data-testid="number"
-              onClick={() => setPage(item)}
-              key={`${item}-button`}
-            >
-              {item}
-            </p>
-          );
+    const updatePages = () => {
+        if (total < 6) {
+            const result = Array.from(Array(total).keys())
+            return result.map((i) => ++i)
+        } else if (page < 4) {
+            return [1, 2, 3, 4, 5, '...']
+        } else if (page > total - 3) {
+            return ['...', total - 4, total - 3, total - 2, total - 1, total]
         } else {
-          return <p className="elipses" key={`${item}-${index}`}>{item}</p>;
+            return ['...', page - 2, page - 1, page, page + 1, page + 2, '...']
         }
-      })}
-      {page === total ? (
-        <div className="spacer" />
-      ) : (
-        <span>
-          <i className="fas fa-chevron-right" onClick={() => setPage(++page)} data-testid="next" />
-          <i
-            className="fas fa-chevron-double-right"
-            onClick={() => setPage(total)}
-            data-testid="jump-end"
-          />
-        </span>
-      )}
-      <style global="true" jsx="true">
-        {`
-          .pagination {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            -webkit-touch-callout: none; /* iOS Safari */
-            -webkit-user-select: none; /* Safari */
-            -khtml-user-select: none; /* Konqueror HTML */
-            -moz-user-select: none; /* Old versions of Firefox */
-            -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none;
-            height: 30px;
-          }
-          .pagination i,
-          .pagination p {
-            padding: 5px 10px;
-            cursor: pointer;
-            color: #7C2CDD;
-            height: 100%;
-            border-radius: 5px;
-          }
+    }
 
-          .pagination i {
-            width: 30px;
-            padding: 5px 0px;
-            text-align: center;
-          }
-          .pagination .active {
-            color: #6A606D;
-            background-color: #F2F2F2 !important;
-          }
+    useEffect(() => {
+        setPages(updatePages())
+    }, [page, total])
 
-          .pagination .elipses {
-            cursor: default;
-            color: #6A606D;
-          }
+    return (
+        <div className='pagination'>
+            {page === 1 ? (
+                <div className='spacer' />
+            ) : (
+                <span>
+                    <i
+                        className='fal fa-chevron-left'
+                        data-testid='previous'
+                        onClick={() => setPage(--page)}
+                    />
+                </span>
+            )}
+            {pages.map((item, index) => {
+                if (typeof item === 'number') {
+                    return (
+                        <p
+                            className={`number ${
+                                page === item ? 'active' : ''
+                            }`}
+                            data-testid='number'
+                            key={`${item}-button`}
+                        >
+                            {item}
+                        </p>
+                    )
+                } else {
+                    return (
+                        <p className='elipses' key={`${item}-${index}`}>
+                            {item}
+                        </p>
+                    )
+                }
+            })}
+            {page === total ? (
+                <div className='spacer' />
+            ) : (
+                <span>
+                    <i
+                        className='fal fa-chevron-right'
+                        data-testid='next'
+                        onClick={() => setPage(++page)}
+                    />
+                </span>
+            )}
+            <style global='true' jsx='true'>
+                {`
+                    .pagination {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        -webkit-touch-callout: none; /* iOS Safari */
+                        -webkit-user-select: none; /* Safari */
+                        -khtml-user-select: none; /* Konqueror HTML */
+                        -moz-user-select: none; /* Old versions of Firefox */
+                        -ms-user-select: none; /* Internet Explorer/Edge */
+                        user-select: none;
+                        height: 30px;
+                    }
+                    .pagination i,
+                    .pagination p {
+                        padding: 5px 10px;
+                        color: #7c2cdd;
+                        height: 100%;
+                        border-radius: 5px;
+                    }
 
-          .pagination .number:hover,
-          .pagination i:hover {
-            background-color: #f2f2f2;
-          }
+                    .pagination i {
+                        width: 30px;
+                        cursor: pointer;
+                        padding: 5px 0px;
+                        text-align: center;
+                    }
+                    .pagination .active {
+                        color: #6a606d;
+                        background-color: #f2f2f2 !important;
+                    }
 
-          .pagination .spacer {
-            width: 60px;
-          }
-        `}
-      </style>
-    </div>
-  );
-};
+                    .pagination .elipses {
+                        cursor: default;
+                        color: #6a606d;
+                    }
+
+                    .pagination i:hover {
+                        background-color: #f2f2f2;
+                    }
+
+                    .pagination .spacer {
+                        width: 60px;
+                    }
+                `}
+            </style>
+        </div>
+    )
+}
 
 Pagination.propTypes = {
-  page: PropTypes.number.isRequired,
-  setPage: PropTypes.func.isRequired,
-  total: PropTypes.number.isRequired
-};
+    page: PropTypes.number.isRequired,
+    setPage: PropTypes.func.isRequired,
+    total: PropTypes.number.isRequired
+}
 
-export default Pagination;
+export default Pagination
