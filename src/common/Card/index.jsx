fcdbef91c404b87999d6296ed52d0e2b2b967669
Austin Zani <austinzani@gmail.com>
2021-09-17 13:53:33 -0400

    updated changes to card

--- src/common/Card/index.jsx
+++ src/common/Card/index.jsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React from 'react'
 
-const Card = ({ border, grey, gradient, width, children }) => {
-  const style = () => {
-    const result = {};
-    if (border) result.borderColor = `var(--${border})`;
-    if (grey) result.backgroundColor = "var(--grey-3)";
-    if (gradient)
-      result.backgroundImage =
-        "url(https://books-ui-assets.s3.amazonaws.com/neutral_gradient_1.svg)";
-    if (width) result.width = width;
-    return result;
-  };
+const Card = ({
+    border,
+    grey,
+    gradient,
+    width,
+    children,
+    borderRadius,
+    className
+}) => {
+    const style = () => {
+        const result = {}
+        if (border) result.borderColor = `var(--${border})`
+        if (grey) result.backgroundColor = 'var(--grey-3)'
+        if (gradient)
+            result.backgroundImage =
+                'url(https://books-ui-assets.s3.amazonaws.com/neutral_gradient_1.svg)'
+        if (width) result.width = width
+        if (borderRadius) result.borderRadius = borderRadius
+        return result
+    }
 
-  return (
-    <div className="pt-card" data-testid="CardRow" style={style()}>
-      {children}
-      <style jsx="true">{`
-        .pt-card {
-          display: flex;
-          flex-direction: column;
-          background-color: var(--white);
-          border-radius: 20px;
-          border: 1px solid transparent;
-          flex-wrap: wrap;
-          padding: 8px;
-          background-size: cover;
-        }
-      `}</style>
-    </div>
-  );
-};
+    return (
+        <div
+            className={`pt-card ${className}`}
+            data-testid='CardRow'
+            style={style()}
+        >
+            {children}
+            <style jsx='true'>
+                {`
+                    .pt-card {
+                        display: flex;
+                        flex-direction: column;
+                        background-color: var(--white);
+                        border-radius: 20px;
+                        border: 1px solid transparent;
+                        flex-wrap: wrap;
+                        padding: 8px;
+                        background-size: cover;
+                    }
+                `}
+            </style>
+        </div>
+    )
+}
 
-export default Card;
+export default Card
