30fe5b9152ce0dd02609cf372c8a79707ee5e359
Austin Zani <austinzani@gmail.com>
2021-09-17 09:18:14 -0400

    updated action col to not show the tooltip if the action is disabled

--- src/common/InnerTable/children/ActionCol.jsx
+++ src/common/InnerTable/children/ActionCol.jsx
@@ -1,38 +1,47 @@
-import React from "react";
-import PropTypes from "prop-types";
-import IconButton from "../../IconButton";
+import React from 'react'
+import PropTypes from 'prop-types'
+import IconButton from '../../IconButton'
 
-const ActionCol = ({ className, row, col, label, icon, action, rowObject, disabled }) => {
-  return (
-    <td
-      className={`cell action ${className}`}
-      key={`${className}-${row}-${col}`}
-      data-testid="unlinked-column"
-    >
-      <span className="content">
-        <IconButton
-          label={label}
-          icon={icon}
-          top
-          onClick={() => {
-            action(rowObject);
-          }}
-          disabled={disabled}
-        />
-      </span>
-    </td>
-  );
-};
+const ActionCol = ({
+    className,
+    row,
+    col,
+    label,
+    icon,
+    action,
+    rowObject,
+    disabled
+}) => {
+    return (
+        <td
+            className={`cell action ${className}`}
+            data-testid='unlinked-column'
+            key={`${className}-${row}-${col}`}
+        >
+            <span className='content'>
+                <IconButton
+                    disabled={disabled}
+                    icon={icon}
+                    label={disabled ? '' : label}
+                    onClick={() => {
+                        action(rowObject)
+                    }}
+                    top
+                />
+            </span>
+        </td>
+    )
+}
 
 ActionCol.propTypes = {
-  className: PropTypes.string.isRequired,
-  col: PropTypes.number.isRequired,
-  label: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
-  action: PropTypes.func.isRequired,
-  row: PropTypes.number.isRequired,
-  rowObject: PropTypes.object.isRequired,
-  disabled: PropTypes.bool
-};
+    className: PropTypes.string.isRequired,
+    col: PropTypes.number.isRequired,
+    label: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    action: PropTypes.func.isRequired,
+    row: PropTypes.number.isRequired,
+    rowObject: PropTypes.object.isRequired,
+    disabled: PropTypes.bool
+}
 
-export default ActionCol;
+export default ActionCol
