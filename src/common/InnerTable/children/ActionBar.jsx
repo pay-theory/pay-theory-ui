e1e23ade8f4518fb84f35165a0d79dc3794aadd7
Austin Zani <austinzani@gmail.com>
2021-09-20 14:08:36 -0400

    updated the way action bar checkbox adds all elements

--- src/common/InnerTable/children/ActionBar.jsx
+++ src/common/InnerTable/children/ActionBar.jsx
@@ -1,94 +1,96 @@
-import React, { useContext, useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import Checkbox from "../../Checkbox";
-import IconButton from "../../IconButton";
-import { TableContext } from "../index";
-import Pagination from "../../Pagination";
+import React, { useContext, useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import Checkbox from '../../Checkbox'
+import IconButton from '../../IconButton'
+import { TableContext } from '../index'
+import Pagination from '../../Pagination'
 
 const getAllItems = (rows) => {
-  const allItems = {};
-  rows.forEach((row, index) => {
-    allItems[`row${index}`] = row.item;
-  });
-  return allItems;
-};
+    const allItems = {}
+    rows.forEach((row, index) => {
+        allItems[`row${index}`] = row.item
+    })
+    return allItems
+}
 
 const ActionBar = ({ actions, rows, paginationHook }) => {
-  const [allItems] = useState(getAllItems(rows));
-  const context = useContext(TableContext);
-  const [indeterminate, setIndeterminate] = useState(false);
+    const context = useContext(TableContext)
+    const [indeterminate, setIndeterminate] = useState(false)
 
-  const actionButtons = actions.map((item, index) => {
+    const actionButtons = actions.map((item, index) => {
+        return (
+            <IconButton
+                bottom
+                disabled={Object.keys(context.selected).length === 0}
+                icon={item.icon}
+                key={`${item.icon}-${index}`}
+                label={
+                    Object.keys(context.selected).length === 0 ? '' : item.label
+                }
+                onClick={() => {
+                    item.action(Object.values(context.selected))
+                }}
+            />
+        )
+    })
+
+    useEffect(() => {
+        if (
+            Object.keys(context.selected).length > 0 &&
+            Object.keys(context.selected).length !== rows.length
+        ) {
+            setIndeterminate(true)
+        } else {
+            setIndeterminate(false)
+        }
+    }, [context.selected, rows])
+
+    const onChange = (e) => {
+        if (e.target.checked) {
+            context.setSelected(getAllItems(rows))
+        } else {
+            context.setSelected({})
+        }
+    }
+
     return (
-      <IconButton
-        label={Object.keys(context.selected).length === 0 ? "" : item.label}
-        icon={item.icon}
-        bottom
-        key={`${item.icon}-${index}`}
-        disabled={Object.keys(context.selected).length === 0}
-        onClick={() => {
-          item.action(Object.values(context.selected));
-        }}
-      />
-    );
-  });
-
-  useEffect(() => {
-    if (
-      Object.keys(context.selected).length > 0 &&
-      Object.keys(context.selected).length !== rows.length
-    ) {
-      setIndeterminate(true);
-    } else {
-      setIndeterminate(false);
-    }
-  }, [context.selected, rows]);
-
-  const onChange = (e) => {
-    if (e.target.checked) {
-      context.setSelected(allItems);
-    } else {
-      context.setSelected({});
-    }
-  };
-
-  return (
-    <span className="action-bar" data-testid="action-bar">
-      {actions.length > 0 ? (
-        <div className="actions">
-          <Checkbox
-            id={`checkbox-${context.id}`}
-            indeterminate={indeterminate}
-            inputProps={{
-              "data-testid": "select-item",
-              checked:
-                Object.keys(context.selected).length > 0 &&
-                Object.keys(context.selected).length === rows.length,
-              onChange: onChange
-            }}
-          />
-          {actionButtons}
-        </div>
-      ) : (
-        <div />
-      )}
-      {paginationHook ? (
-        <Pagination paginationHook={paginationHook} />
-      ) : (
-        <div />
-      )}
-    </span>
-  );
-};
+        <span className='action-bar' data-testid='action-bar'>
+            {actions.length > 0 ? (
+                <div className='actions'>
+                    <Checkbox
+                        id={`checkbox-${context.id}`}
+                        indeterminate={indeterminate}
+                        inputProps={{
+                            'data-testid': 'select-item',
+                            checked:
+                                Object.keys(context.selected).length > 0 &&
+                                Object.keys(context.selected).length ===
+                                    rows.length,
+                            onChange: onChange
+                        }}
+                    />
+                    {actionButtons}
+                </div>
+            ) : (
+                <div />
+            )}
+            {paginationHook ? (
+                <Pagination paginationHook={paginationHook} />
+            ) : (
+                <div />
+            )}
+        </span>
+    )
+}
 
 ActionBar.propTypes = {
-  rows: PropTypes.array.isRequired,
-  actions: PropTypes.array,
-  paginationHook: PropTypes.object
-};
+    rows: PropTypes.array.isRequired,
+    actions: PropTypes.array,
+    paginationHook: PropTypes.object
+}
 
 ActionBar.defaultProps = {
-  actions: []
-};
+    actions: []
+}
 
-export default ActionBar;
+export default ActionBar
